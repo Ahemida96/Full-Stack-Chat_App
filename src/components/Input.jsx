659e32e4dc9aebda6f09684ce447b7502ddfca76
Ahmed Hemida <ahmedhemida121@gmail.com>
2023-12-23 20:34:30 +0200

    Update Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,7 +3,7 @@ import attach from '../img/attach.png'
 import img from '../img/img.png'
 import { AuthContext } from '../context/AuthContext'
 import { ChatContext } from '../context/ChatContext'
-import { Timestamp, arrayUnion, doc, serverTimestamp, updateDoc } from 'firebase/firestore'
+import { Timestamp, arrayUnion, doc, serverTimestamp, setDoc, updateDoc } from 'firebase/firestore'
 import { v4 as uuid } from 'uuid'
 import { db, storage } from '../firebase-config'
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
@@ -11,12 +11,15 @@ import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
 const Input = () => {
   const [textMessage, setTextMessage] = useState("")
   const [imgMessage, setImgMessage] = useState(null)
+  const [loading, setLoading] = useState(false);
 
   const {currentUser} = useContext(AuthContext)
   const {data} = useContext(ChatContext)
 
+
   const handleSend= async ()=> {
     if (imgMessage){
+      setLoading(true);
       const storageRef = ref(storage, uuid());
 
       const uploadTask = uploadBytesResumable(storageRef, imgMessage);
@@ -39,8 +42,7 @@ const Input = () => {
           });
         }
       );
-
-    }else{
+    }else if (textMessage){
       await updateDoc(doc(db, "chats", data.chatId),{
         messages: arrayUnion({
           id: uuid(),
@@ -50,30 +52,82 @@ const Input = () => {
         }),
       });
     }
-    await updateDoc(doc(db, "userChats", currentUser.uid), {
-      [data.chatId + ".lastMessage"]: {
-        text:textMessage,
-      },
-      [data.chatId + ".date"]: serverTimestamp(),
-    });
+    const combinedId =
+        currentUser.uid > data.user.uid
+          ? currentUser.uid + data.user.uid
+          : data.user.uid + currentUser.uid;
+    if (textMessage){
+      try{
 
-    await updateDoc(doc(db, "userChats", data.user.uid), {
-      [data.chatId + ".lastMessage"]: {
-        text: textMessage,
-      },
-      [data.chatId + ".date"]: serverTimestamp(),
-    });
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [data.chatId + ".lastMessage"]: {
+            text:textMessage,
+          },
+          [data.chatId + ".date"]: serverTimestamp(),
+        });
+      }catch(err){
+        console.error(err)
+        console.log("err in update CurrentUser userChats")
+
+        await setDoc(doc(db, "userChats", currentUser.uid), {
+              [combinedId]: {
+                userInfo: {
+                  uid: data.user.uid,
+                  displayName: data.user.displayName,
+                  photoURL: data.user.photoURL,
+                },
+                lastMessage: {
+                  text: textMessage,
+                },
+                date: serverTimestamp(),
+              },
+            });
+      };
+      
+      try{
+        await updateDoc(doc(db, "userChats", data.user.uid), {
+          [data.chatId + ".lastMessage"]: {
+            text:textMessage,
+          },
+          [data.chatId + ".date"]: serverTimestamp(),
+        });
+      }catch(err){
+        console.error(err)
+        console.log("err in update otherUser userChats")
+
+        await setDoc(doc(db, "userChats", data.user.uid), {
+          [combinedId]: {
+            userInfo: {
+              uid: currentUser.uid,
+              displayName: currentUser.displayName,
+              photoURL: currentUser.photoURL,
+            },
+            lastMessage: {
+              text: textMessage,
+            },
+            date: serverTimestamp(),
+          },
+        });
+      };
+    
+    }
 
     setTextMessage("");
     setImgMessage(null);
+    setLoading(false);
+  };
+
+  const handleKey = (e) => {
+    e.code === 'Enter' && handleSend();
   };
 
   return (
     <div className='input'>
-      <input type="text" placeholder='Type something...' onChange={e=>setTextMessage(e.target.value)} value={textMessage}/>
+      <input type="text" placeholder='Type something...' onChange={e=>setTextMessage(e.target.value)} value={textMessage} onKeyUp={handleKey}/>
       <div className="send">
         <img src={attach} alt="" />
-        <input type="file" style={{display: "none"}} id='file' onChange={e=>setImgMessage(e.target.files[0])} />
+        <input type="file" style={{display: "none"}} id='file' onChange={e=>setImgMessage(e.target.files[0])} onKeyUp={handleKey}/>
+        {loading && (<div class="loader"></div>)}
         <label htmlFor="file">
           <img src={img} alt="" />
         </label>
