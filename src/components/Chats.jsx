0aa51543d239eda942502c760b790d40aba4c5b6
Ahmed Hemida <ahmedhemida121@gmail.com>
2023-12-23 20:34:10 +0200

    Add condition for displaying the chats

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -29,7 +29,7 @@ const Chats = () => {
 
   return (
     <div className='chats'>
-      {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
+      {chats && (Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
         <div className="userChat" key={chat[0]} onClick={() => handleSelect(chat[1].userInfo)}>
         <img src={chat[1].userInfo.photoURL} alt="" />
         <div className="userChatInfo">
@@ -37,8 +37,9 @@ const Chats = () => {
           <p>{chat[1].lastMessage?.text}</p>
         </div>
       </div>
-      ))}
+      )))}
     </div>
+    // <div>Chats</div>
   )
 }
 
