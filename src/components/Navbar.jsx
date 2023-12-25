9c382f1913b10a05c3493f67d87d8e4ea82f1142
Ahmed Hemida <ahmedhemida121@gmail.com>
2023-12-25 17:09:28 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,20 @@ import React, { useContext } from 'react'
 import {signOut} from 'firebase/auth'
 import {auth} from '../firebase-config'
 import { AuthContext } from '../context/AuthContext'
+// import setOpen from './Profile'
 
 const Navbar = () => {
   const {currentUser} = useContext(AuthContext)
+
   return (
     <div className='navbar'>
-      <span className='logo'>Progen</span>
+      {/* <span className='logo'>Progen</span> */}
       <div className="user">
         <img src={currentUser.photoURL} alt="" />
-        <span>{currentUser.displayName}</span>
-        <button onClick={()=>signOut(auth)}>log out</button>
+        <span className='AddGroup'></span>
+        <span className='logout' onClick={()=>signOut(auth)}></span>
+
+        {/* <h4>{currentUser.displayName}</h4> */}
       </div>
     </div>
   )
