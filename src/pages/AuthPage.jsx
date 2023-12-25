76313c75b8ee3c252ffd898f62b6c210382ecba0
Habeba-Ahmed <114295890+Habeba-Ahmed@users.noreply.github.com>
2023-12-25 23:19:03 +0200

    login and register styling

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -4,6 +4,11 @@ import { useNavigate, Link } from 'react-router-dom';
 import { useState } from 'react';
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser ,faLock} from '@fortawesome/free-solid-svg-icons';
+
+
+
 
 export const AuthPage = () => {
     const [err, setErr] = useState(false);
@@ -32,27 +37,64 @@ export const AuthPage = () => {
     };
 
     return (
-        <div className="formContainer">
-            <div className="formWrapper">
-                {/* <h1>Sign In</h1> */}
-                <span className="logo">Progen Chat</span>
-                <span className="title">Login</span>
-                <form onSubmit={SignInWithEmailAndPass} >
-                    <label for="email">Email</label>
-                    <input type="email" id="email" name="email" required />
-                    <label for="password">Password</label>
-                    <input type="password" id="password" name="password" required />
-                    <a href="/forgot-password">Forgot password?</a>
-                    <button type="submit">Sign In</button>
-                    {err && <span>Something went wrong</span>}
-                    <div class="separator">
-                        <center><span>or</span></center>
-                    </div>
-                    <button onClick={SignInWithGoogle}>Sign In with Google</button>
-                </form>
-                <p>Don't have an account? <Link to='/register'> Sign Up</Link></p>
+        // <div className="formContainer">
+        //     <div className="formWrapper">
+        //         {/* <h1>Sign In</h1> */}
+        //         <span className="logo">Progen Chat</span>
+        //         <span className="title">Login</span>
+        //         {/* <form onSubmit={SignInWithEmailAndPass} >
+        //             <label for="email">Email</label>
+        //             <input type="email" id="email" name="email" placeholder='Name' required />
+        //             <FontAwesomeIcon icon={faUser} color='white' />
+        //             <label for="password">Password</label>
+        //             <input type="password" id="password" name="password" placeholder='password' required />
+        //             <a href="/forgot-password">Forgot password?</a>
+        //             <button type="submit">Sign In</button>
+        //             {err && <span>Something went wrong</span>}
+        //             <div class="separator">
+        //                 <center><span>or</span></center>
+        //             </div>
+        //             <button onClick={SignInWithGoogle}>Sign In with Google</button>
+        //         </form> */}
+                
+        //         <p>Don't have an account? <Link class='btn' to='/register'> Sign Up</Link></p>
+        //     </div>
+        // </div>
+
+        <div class="wrapper">
+        <form action="" onSubmit={SignInWithEmailAndPass}>
+            <h1>Progen Chat</h1>
+            <h2>login</h2>
+            <div class="input-box">
+                <input type="text" placeholder="Name" required/>
+                <FontAwesomeIcon icon={faUser} id='icon'/>
+                
             </div>
-        </div>
+            <div class="input-box">
+                <input type="password" placeholder="password" required/>
+                <FontAwesomeIcon icon={faLock} id='icon'/>
+            </div>
+
+            <div class="remember-forgot">
+                {/* <!-- <label for=""><input type="checkbox">remember me</label> --> */}
+                <a href="# ">Forgot Password?</a>
+            </div>
+
+            <button type="submit" class="btn">login</button>
+            {err && <span>Something went wrong</span>}
+
+            <div class="separator">
+                <center><span>or</span></center>
+            </div>
+            
+            <button onClick={SignInWithGoogle} type="submit" class="btn">login With Google</button>
+            <div class="register-link">
+                <p>Don't have an account ? <Link  to='/register'> Sign Up</Link></p>
+            </div>
+
+
+        </form>
+    </div>
 
     )
 }
