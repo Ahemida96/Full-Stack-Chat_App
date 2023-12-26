abc0831f19e7b3efe37b5b5c0bbfe6ffa33a8391
Ahmed Hemida <ahmedhemida121@gmail.com>
2023-12-26 04:45:55 +0200

    Update Chat.jsx

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,6 +2,9 @@ import React, {useContext, useState} from 'react'
 import Messages from './Messages'
 import Input from './Input'
 import { ChatContext } from '../context/ChatContext'
+import { AuthContext } from '../context/AuthContext'
+import { doc, updateDoc, deleteField } from "firebase/firestore";
+import { db } from '../firebase-config'
 
 import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
@@ -10,32 +13,65 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import CallIcon from '@mui/icons-material/Call';
 import DuoIcon from '@mui/icons-material/Duo';
 import Stack from '@mui/material/Stack';
-
-// import video from '../img/cam.png'
-// import call from '../img/icons8-call-32.png'
-// import add from '../img/add.png'
-
-const options = [
-  'Profile',
-  'Delete Chat',
-  'Mute Notifications',
-  'Wallpaper',
-  'Search'
-];
+import Snackbar from '@mui/material/Snackbar';
+import CloseIcon from '@mui/icons-material/Close';
+import Button from '@mui/material/Button';
 
 const ITEM_HEIGHT = 48;
 
 const Chat = () => {
   const { data } = useContext(ChatContext);
+  const { currentUser } = useContext(AuthContext);
+  const [openSnackbar, setOpenSnackbar] = useState(false); // for snackbar
 
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
+
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
-  const handleClose = () => {
+
+  const handleClose= () => {
     setAnchorEl(null);
   };
+
+  const handleSnackbarClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpenSnackbar(false);
+  };
+
+  const deleteChat = async() => {
+    try{
+      const messagesRef = doc(db, 'chats', data.chatId);
+      updateDoc(messagesRef, {
+        [currentUser.uid]: deleteField(),
+        [currentUser.uid]:{
+          messages: [],
+        }
+      });
+      setOpenSnackbar(true);
+    }catch(err){
+      console.log(err);
+    }
+    setAnchorEl(null);
+  }
+  const action = (
+    <React.Fragment>
+      <Button color="secondary" size="small" onClick={handleSnackbarClose}>
+        UNDO
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleSnackbarClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
 
   return (
     <div className='chat'>
@@ -77,15 +113,23 @@ const Chat = () => {
                 },
               }}
             >
-              {options.map((option) => (
-                <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
-                  {option}
-                </MenuItem>
-              ))}
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>Mute Notifications</MenuItem>
+                <MenuItem onClick={handleClose}>Search</MenuItem>
+                <MenuItem onClick={handleClose}>Wallpaper</MenuItem>
+                <MenuItem onClick={deleteChat} style={{color: "red"}}>Delete Chat</MenuItem>
+
             </Menu>
           </Stack>
         </div>
       </div>
+        <Snackbar
+          open={openSnackbar}
+          autoHideDuration={6000}
+          onClose={handleSnackbarClose}
+          message="Chat Deleted Successfully"
+          action={action}
+          />
         <Messages />
         <Input />
     </div>
