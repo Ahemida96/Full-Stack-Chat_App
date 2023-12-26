2d8920943ab88d85797b7f0d2186e15a9d83cfce
Ahmed Hemida <ahmedhemida121@gmail.com>
2023-12-26 04:46:09 +0200

    Update Messages.jsx

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -3,14 +3,17 @@ import Message from './Message'
 import { ChatContext } from '../context/ChatContext';
 import { doc, onSnapshot } from 'firebase/firestore';
 import { db } from '../firebase-config';
+import { AuthContext } from '../context/AuthContext';
 
 const Messages = () => {
   const [messages, setMessages] = useState([])
   const { data } = useContext(ChatContext);
+  const { currentUser } = useContext(AuthContext);
 
   useEffect(()=>{
     const unsub = onSnapshot(doc(db, "chats", data.chatId), (doc)=> {
-      doc.exists() && setMessages(doc.data().messages);
+      doc.exists() && setMessages(doc.data()[currentUser.uid].messages);
+      // console.log(doc.data()[currentUser.uid].messages);
     });
     return ()=> {
       unsub();
