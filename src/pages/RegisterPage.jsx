98b9a679d34d6c8e261cf4e0da0c0ac8e9bf98c9
Ahmed Hemida <ahmedhemida121@gmail.com>
2024-01-05 07:08:38 +0200

    fix registration bugs

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,17 +1,19 @@
 import {React, useState} from 'react'
-import {auth, db, storage} from '../firebase-config';
+import {auth, db } from '../firebase-config';
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
 import { useNavigate, Link } from 'react-router-dom';
-// import Add from '../img/addAvatar.png'
-import defultImg from '../img/broken-image.png'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser ,faLock , faPhone ,faEnvelope} from '@fortawesome/free-solid-svg-icons';
 
 
 export const RegisterPage = () => {
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [password, setPassword] = useState("");
+
   const [err, setErr] = useState(false);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
@@ -19,34 +21,27 @@ export const RegisterPage = () => {
   const Register = async (e) => {
     setLoading(true);
     e.preventDefault();
-    const displayName = e.target[0].value;
-    const email = e.target[1].value;
-    const password = e.target[2].value;
-    const img = e.target[3].files[0] || defultImg;
-    console.log(displayName, email, password, img);
+
+    console.log(displayName, email, password , phoneNumber);
 
     try {
       console.log("Registering...");
       const res = await createUserWithEmailAndPassword(auth, email, password);
-      console.log(res.user);
-      //Create a unique image name
-      const date = new Date().getTime();
-      const storageRef = ref(storage, `${displayName + date}`);
-
-      await uploadBytesResumable(storageRef, img).then(() => {
-        getDownloadURL(storageRef).then(async (downloadURL) => {
+      // console.log(res.user);
           try {
             //Update profile
             await updateProfile(res.user, {
               displayName,
-              photoURL: downloadURL,
+              photoURL: null,
             });
             //create user on firestore
             await setDoc(doc(db, "users", res.user.uid), {
               uid: res.user.uid,
               displayName,
               email,
-              photoURL: downloadURL,
+              photoURL: null,
+              phoneNumber,
+              friends: [],
             });
 
             //create empty user chats on firestore
@@ -57,8 +52,6 @@ export const RegisterPage = () => {
             setErr(true);
             setLoading(false);
           }
-        });
-      });
       } catch (err) {
       setErr(true);
       console.log(err);
@@ -70,21 +63,21 @@ export const RegisterPage = () => {
     <div className="auth-regpage">
       <div class="wrapper">
         <h2 className="title">Registeration</h2> 
-        <form onClick={Register}>
+        <form onSubmit={Register}>
           <div class="input-box">
-                  <input type="text" placeholder="Username" required/>
+                  <input type="text" placeholder="Username" required onChange={(e) => setDisplayName(e.target.value)}/>
                   <FontAwesomeIcon icon={faUser} id='icon' />
           </div>
           <div class="input-box">
-                  <input type="email" placeholder="Email" required/>
+                  <input type="email" placeholder="Email" required onChange={(e)=>setEmail(e.target.value)}/>
                   <FontAwesomeIcon icon={faEnvelope} id='icon' />
           </div>
           <div class="input-box">
-                  <input type="number" placeholder="Phone Number" required/>
+                  <input type="number" placeholder="Phone Number" required onChange={(e)=> setPhoneNumber(e.target.value)}/>
                   <FontAwesomeIcon icon={faPhone} id='icon' />
           </div>
           <div class="input-box">
-                  <input type="password" placeholder="password" required/>
+                  <input type="password" placeholder="password" required onChange={(e)=> setPassword(e.target.value)}/>
                   <FontAwesomeIcon icon={faLock}  id='icon' />
 
           </div>
@@ -93,7 +86,6 @@ export const RegisterPage = () => {
                   <FontAwesomeIcon icon={faLock} id='icon'/>
           </div>
 
-          {/* <label for=""><input type="checkbox"/> I heraby declare the above information provided is true and correct</label> */}
           <button type="submit" class="btn">Sign up</button>
           {loading && <span className='loader'> "Uploading and compressing the image please wait..."</span>}
           {err && <span style={{color: 'red'}}>Something went wrong</span>}
