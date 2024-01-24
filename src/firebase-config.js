2f715a17ec79a31d29d0478d2c6ee361deca798b
Ahmed Hemida <ahmedhemida121@gmail.com>
2024-01-24 10:29:22 +0200

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,16 +3,37 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
-
+/// gmail | progen
 // Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyChWneq6pD22BA0UaPlHyJm2ByWUE_j6Yg",
+//   authDomain: "progen-9fa96.firebaseapp.com",
+//   projectId: "progen-9fa96",
+//   storageBucket: "progen-9fa96.appspot.com",
+//   messagingSenderId: "772709933820",
+//   appId: "1:772709933820:web:f0b7cd28a31839ee00ad79"
+// };
+
+// gmail | chatapp
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDLl5uN9C6YyLmmE3bu5JEn98PTebYQCtU",
+//   authDomain: "chatapp-f7523.firebaseapp.com",
+//   projectId: "chatapp-f7523",
+//   storageBucket: "chatapp-f7523.appspot.com",
+//   messagingSenderId: "394662213406",
+//   appId: "1:394662213406:web:20c4d1334fb678028d7b0f"
+// };
+
+// pua | chatapp
 const firebaseConfig = {
-  apiKey: "AIzaSyChWneq6pD22BA0UaPlHyJm2ByWUE_j6Yg",
-  authDomain: "progen-9fa96.firebaseapp.com",
-  projectId: "progen-9fa96",
-  storageBucket: "progen-9fa96.appspot.com",
-  messagingSenderId: "772709933820",
-  appId: "1:772709933820:web:f0b7cd28a31839ee00ad79"
+  apiKey: "AIzaSyDXy9lv9X904fqH1hgzy-K1Hw9qcHMjX0g",
+  authDomain: "chatapp-3ce01.firebaseapp.com",
+  projectId: "chatapp-3ce01",
+  storageBucket: "chatapp-3ce01.appspot.com",
+  messagingSenderId: "645420705950",
+  appId: "1:645420705950:web:9f9a44de5365a8e29b738f"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
